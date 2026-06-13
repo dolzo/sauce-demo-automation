a0dfb5f348fabaa2da1e01cc39f22d3626cb5dd0
dolzo <degolxz01@gmail.com>
2026-06-12 20:22:22 -0400

    feat: agregar el elemento div del container de productos a login.py

--- pages/page_objects/login.py
+++ pages/page_objects/login.py
@@ -5,3 +5,4 @@ class Login:
     input_username = "//input[@id='user-name']"
     input_password = "//input[@id='password']"
     button_login = "//input[@id='login-button']"
+    div_inventory_container = "//div[@id='inventory_container']"
