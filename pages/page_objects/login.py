560435e15bdf26c617bf7c21edb3b5cbdb384960
dolzo <degolxz01@gmail.com>
2026-06-13 13:34:44 -0400

    fix: corregidas las tuplas de localizador, metodo by en login.py

--- pages/page_objects/login.py
+++ pages/page_objects/login.py
@@ -1,8 +1,7 @@
 from selenium.webdriver.common.by import By
 
 class Login:
-    
-    input_username = "//input[@id='user-name']"
-    input_password = "//input[@id='password']"
-    button_login = "//input[@id='login-button']"
-    div_inventory_container = "//div[@id='inventory_container']"
+    input_username = (By.XPATH, "//input[@id='user-name']")
+    input_password = (By.XPATH, "//input[@id='password']")
+    button_login = (By.XPATH, "//input[@id='login-button']")
+    div_inventory_container = (By.XPATH, "//div[@id='inventory_container']")
