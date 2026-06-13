1b400a30f335da130aaba2fcfc8b2796f1222f2d
dolzo <degolxz01@gmail.com>
2026-06-12 20:13:20 -0400

    feat: crear primera version del page object para la funcion de login

---
+++ pages/page_objects/login.py
@@ -0,0 +1,7 @@
+from selenium.webdriver.common.by import By
+
+class Login:
+    
+    input_username = "//input[@id='user-name']"
+    input_password = "//input[@id='password']"
+    button_login = "//input[@id='login-button']"
